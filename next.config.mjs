b779fce61e4ba7b519cd75cb8b46e5cdbdd0ab45
Geoffrey Wu <38195698+gwu205@users.noreply.github.com>
2024-12-09 23:34:24 +0900

    Set cache-control headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,41 @@
 import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    swcPlugins: [["@onlook/nextjs", {
-      root: path.resolve(".")
-    }]]
-  }
+    swcPlugins: [
+      [
+        "@onlook/nextjs",
+        {
+          root: path.resolve("."),
+        },
+      ],
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*", // Apply to all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://www.geoffreywu.digital",
+        permanent: true,
+      },
+    ];
+  },
 };
+
 export default nextConfig;
